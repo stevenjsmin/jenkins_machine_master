84669ce12ef0d9e56a123c4ea0782f8e2c15d30c
Steven J.S Min <stevenmin@nbnco.com.au>
2019-05-27 17:44:30 +1000

    Project init

--- packer/cicd-tools/appianci.sh
+++ packer/cicd-tools/appianci.sh
@@ -6,4 +6,4 @@ export JENKINS_HOME=/var/lib/jenkins
 
 export PATH=.:/opt/packer/:$PATH:$JAVA_HOME/bin:$M2_HOME/bin
 
-export PS1="\u@\e[0;32mNBN-CICD \e[m\e[0;36m\w \e[m$ "
+export PS1="\u@\e[0;32mNBN-CICD-Master \e[m\e[0;36m\w \e[m$ "
